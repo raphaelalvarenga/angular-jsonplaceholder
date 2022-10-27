dbfb073b3f880630aefb134b4d9b937d417fbd3f
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-10-27 13:55:28 -0300

    Created the components that will be work as routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { PhotosComponent } from './photos/photos.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CommentsComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
