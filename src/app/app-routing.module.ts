7227fb344aa832a53c7acdac2664c596a5d2cd7c
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-10-27 14:03:21 -0300

    Routes defined.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { CommentsComponent } from './comments/comments.component';
+import { PhotosComponent } from './photos/photos.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'comments', component: CommentsComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'users', component: UsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
